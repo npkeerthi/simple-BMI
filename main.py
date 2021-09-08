ce799334c1eb89740e63a31a2978d44a904ed2c1
N Padma Keerthi <80039790+npkeerthi@users.noreply.github.com>
2021-09-08 19:55:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+
+heigh = input("enter your height in m: ")
+weigh = input("enter your weight in kg: ")
+
+height=float(heigh)
+weight=int(weigh)
+
+BMI=(weight/height**2)
+print("Your BMI is",BMI)
+
+
+
+
+
+
+
+
+
+
+
